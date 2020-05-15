8ec701ff06a06e07ebc4ac151e151afaf539dc5b
RadHatter <aar.j.smith@gmail.com>
2020-05-14 22:16:40 -0500

    Fortress Physics
    Implemented a prototype fortress for testing purposes including some basic physics.

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class GameMaster : MonoBehaviour
 {
+    [SerializeField] private Camera fortressCam = null;
+    private float cameraHeight;
+    private float cameraWidth;
+
+    [SerializeField] private PlayerShip playerShip = null;
+
+    private void Awake()
+    {
+        cameraHeight = fortressCam.orthographicSize * 2;
+        cameraWidth = cameraHeight * fortressCam.aspect;
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +24,29 @@ public class GameMaster : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        PerformBoundsCheck();
+    }
+
+    private void PerformBoundsCheck()
+    {
+        Vector3 playerPosition = playerShip.transform.position;
+        if (playerPosition.x < 0)
+        {
+            playerPosition.x = cameraWidth;
+        }
+        else if (playerPosition.x > cameraWidth)
+        {
+            playerPosition.x = 0;
+        }
+
+        if (playerPosition.y < 0)
+        {
+            playerPosition.y = cameraHeight;
+        }
+        else if (playerPosition.y > cameraHeight)
+        {
+            playerPosition.y = 0;
+        }
+        playerShip.transform.position = playerPosition;
     }
 }
--- Assets/Scripts/PlayerShip.cs
+++ Assets/Scripts/PlayerShip.cs
@@ -9,17 +9,15 @@ using UnityEngine.SocialPlatforms;
 
 public class PlayerShip : MonoBehaviour
 {
-    [SerializeField] private GameObject[] shipTrackers = new GameObject[2];
-    private Transform noseTransform;
-    private Transform tailTransform;
-
     private GameControls gameControls;
+    [SerializeField] private Rigidbody2D shipBody = null;
 
     private float rotationFactor = 0f;
-    private float rotationSpeed = 100f;
+    private float rotationSpeed = 120f;
 
     private Vector3 velocity = Vector3.zero;
-    private float maxVelocity = 40f;
+    private float maxVelocity = 25f;
+    private float velocityDecayRate = 0.998750f;
 
     private bool thrustersReady = true;
     private bool thrustersEngaged = false;
@@ -29,11 +27,8 @@ public class PlayerShip : MonoBehaviour
 
     private void Awake()
     {
-        noseTransform = shipTrackers[0].transform;
-        tailTransform = shipTrackers[1].transform;
         gameControls = new GameControls();
-        gameControls.InGameControls.ToggleThruster.performed += ctx => thrustersEngaged = !thrustersEngaged;
-        gameControls.InGameControls.RotateShip.performed += ctx => rotationFactor = ctx.ReadValue<float>();
+        gameControls.InGameControls.ToggleThruster.performed += toggleThruster => thrustersEngaged = !thrustersEngaged;
     }
 
     // Start is called before the first frame update
@@ -56,41 +51,63 @@ public class PlayerShip : MonoBehaviour
     void Update()
     {
         float deltaTime = Time.deltaTime;
-        MoveShip(deltaTime);
-
-        if(thrustersEngaged)
-            Debug.Log(thrustersEngaged);
-    }
-
-    private void MoveShip(float deltaTime)
-    {
-        
-        if (Mathf.Abs(rotationFactor) > 0.1f)
-            transform.Rotate(new Vector3(0f, 0f, rotationFactor * rotationSpeed) * deltaTime);
-        
-        if(!thrustersReady)
-        {
-            thrusterCooldown += deltaTime;
-            if(thrusterCooldown >= thrusterInterval)
-            {
-                thrustersReady = true;
-                thrusterCooldown = 0f;
-            }
-        }
-        else if(thrustersReady && thrustersEngaged)
-        {
-            thrustersReady = false;
-            velocity += GetThrustVelocity() * thrusterPower;
-            if (velocity.magnitude > maxVelocity)
-                velocity = velocity.normalized * maxVelocity;
-        }
+        CheckRotation(deltaTime);
+        CheckThrusters(deltaTime);
         transform.Translate(velocity * deltaTime, Space.World);
     }
 
-    private Vector3 GetThrustVelocity()
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        BounceBack();
+    }
+
+    private void CheckRotation(float deltaTime)
+    {
+        rotationFactor = gameControls.InGameControls.RotateShip.ReadValue<float>();
+        if (Mathf.Abs(rotationFactor) > 0.2f)
+            transform.Rotate(new Vector3(0f, 0f, rotationFactor * rotationSpeed) * deltaTime);    
+    }
+
+    private void CheckThrusters(float deltaTime)
+    {
+        if (thrustersEngaged)
+        {
+            if (thrustersReady)
+            {
+                thrustersReady = false;
+                velocity += GetThrustVector() * thrusterPower;
+                if (velocity.magnitude > maxVelocity)
+                    velocity = velocity.normalized * maxVelocity;
+            }
+            else
+            {
+                thrusterCooldown += deltaTime;
+                if (thrusterCooldown >= thrusterInterval)
+                {
+                    thrustersReady = true;
+                    thrusterCooldown = 0f;
+                }
+            }
+        }
+        else
+        {
+            if (velocity.magnitude > 0.25f)
+                velocity = (velocity.magnitude * velocityDecayRate) * velocity.normalized;
+            else
+                velocity = Vector3.zero;
+        }
+    }
+
+    private Vector3 GetThrustVector()
     {
         float thrustY = Mathf.Tan(transform.rotation.z * Mathf.Deg2Rad);
         float thrustX = 1f;
-        return transform.TransformDirection(new Vector3(thrustX, thrustY, 0f));
+        return transform.TransformDirection(new Vector3(thrustX, thrustY, 0f)).normalized;
+    }
+
+    private void BounceBack()
+    {
+        transform.Translate(-velocity.normalized * .1f);
+        velocity *= -0.33f;        
     }
 }
